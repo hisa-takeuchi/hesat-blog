29ba63f11c4478ae9f239bf8921c129005092c2a
takeuchi <>
2023-05-26 16:28:12 +0900

    add:ヘッダー

--- app.vue
+++ app.vue
@@ -1,7 +1,8 @@
 <template>
   <NuxtLayout>
+    <TheHeader />
     <div class="mx-auto max-w-7xl px-5 sm:px-8 lg:px-10">
-      <div class="mx-auto max-w-3xl py-10 md:py-16">
+      <div class="mx-auto max-w-3xl py-28">
         <NuxtPage />
       </div>
     </div>
@@ -13,3 +14,5 @@ html {
   font-family: 'Noto Sans JP', sans-serif;
 }
 </style>
+<script setup lang="ts">
+</script>
